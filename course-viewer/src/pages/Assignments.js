b43ee436ca3d2f9d44e1280bbca536c6dfcbb27f
wanfeijong <wanfeichong@gmail.com>
2023-03-16 15:21:06 +0800

    inital structure for general assignments view page

--- course-viewer/src/pages/Assignments.js
+++ course-viewer/src/pages/Assignments.js
@@ -1,7 +1,26 @@
-import React from "react";
+import * as React from "react";
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import AssignmentTable from "./Assignment/components/AssignmentTable/AssignmentTable";
+import "./Assignment/AssignmentInstructor.css";
 
 const Assignments = () => {
-    return <h1>Assignments</h1>
-}
-
+    return (
+        <div>
+            <Box>
+                <Toolbar />
+                <Container maxWidth="lg" sx={{ mt: 10, mb: 4 }}>
+                    <Grid container spacing={10}>
+                    {/* Recent Orders */}
+                    <Grid item xs={20}>
+                        <AssignmentTable />
+                    </Grid>
+                    </Grid>
+                </Container>
+                </Box>
+        </div>
+    )
+  };
 export default Assignments;
