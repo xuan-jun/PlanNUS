d234f2ba25599ef6f67a3b0df1ac96dd7f1a63a9
kohlongyang <longyangk10@gmail.com>
2023-03-21 01:26:49 +0800

    edit add new page

--- course-viewer/src/App.js
+++ course-viewer/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Login from './pages/Login'
 import Calendar from './pages/Calendar'
 import Assignments from './pages/Assignments'
+import AddAssignment from './pages/AddAssignment';
 import { Route, Routes } from "react-router-dom"
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<Login/>}></Route>
           <Route path='/calendar' element={<Calendar/>}></Route>
           <Route path='/assignments' element={<Assignments/>}></Route>
+          <Route path='/assignments/addnew' element={<AddAssignment/>}></Route>
         </Routes>
       </div>
     </>
