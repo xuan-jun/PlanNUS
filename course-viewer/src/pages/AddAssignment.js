e76e809d01ccb1c4a613a13485d447c73cd8ff1d
kohlongyang <longyangk10@gmail.com>
2023-03-21 00:05:57 +0800

    edit add new page

--- course-viewer/src/pages/AddAssignment.js
+++ course-viewer/src/pages/AddAssignment.js
@@ -1,4 +1,41 @@
 import React from "react";
 import AssignmentForm from "./components/AssignmentForm/AssignmentForm";
 
-<h2 Add New Assignment /h2>
+export default function Assignments() {
+
+    const classes = useStyles();
+    const [recordForEdit, setRecordForEdit] = useState(null)
+    const [records, setRecords] = useState(assignmentService.getAllAssignments())
+    const [filterFn, setFilterFn] = useState({ fn: items => { return items; } })
+    const [openPopup, setOpenPopup] = useState(false)
+
+    const {
+        TblContainer,
+        TblHead,
+        TblPagination
+    } = useTable(records, headCells, filterFn);
+
+
+    const addOrEdit = (assignment, resetForm) => {
+        if (assignment.id === 0)
+            assignmentService.insertAssignment(assignment)
+        else
+            assignmentService.updateAssignment(assignment)
+        resetForm()
+        setRecordForEdit(null)
+        setOpenPopup(false)
+        setRecords(assignmentService.getAllAssignments())
+    }
+
+    const handleCellClick = (item) => {
+        openInPopup(item);
+    }
+
+    return (
+        <>
+            <AssignmentForm
+                recordForEdit={recordForEdit}
+                addOrEdit={addOrEdit} />
+        </>
+        
+    )
