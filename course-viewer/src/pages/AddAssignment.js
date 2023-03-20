8474cd4d00b38f6c70329452cece065735ef6ed0
kohlongyang <longyangk10@gmail.com>
2023-03-21 00:08:43 +0800

    edit add new page

--- course-viewer/src/pages/AddAssignment.js
+++ course-viewer/src/pages/AddAssignment.js
@@ -1,41 +1,101 @@
-import React from "react";
-import AssignmentForm from "./components/AssignmentForm/AssignmentForm";
+import React, { useState, useEffect } from "react";
+import { Grid } from "@material-ui/core";
+import Controls from "../../components/controls/Controls";
+import { useForm, Form } from "../../components/useForm";
 
-export default function Assignments() {
-
-    const classes = useStyles();
-    const [recordForEdit, setRecordForEdit] = useState(null)
-    const [records, setRecords] = useState(assignmentService.getAllAssignments())
-    const [filterFn, setFilterFn] = useState({ fn: items => { return items; } })
-    const [openPopup, setOpenPopup] = useState(false)
-
+const initialFValues = {
+    id: 0,
+    AssignmentName: "",
+    startDate: new Date(),
+    endDate: new Date(),
+    weightage: ""
+  };
+  
+  export default function AssignmentForm(props) {
+    const { addOrEdit, recordForEdit } = props;
+  
+    const validate = (fieldValues = values) => {
+      let temp = { ...errors };
+      if ("AssignmentName" in fieldValues)
+        temp.AssignmentName = fieldValues.AssignmentName
+          ? ""
+          : "This field is required.";
+      if ("startDate" in fieldValues)
+        temp.startDate = fieldValues.startDate ? "" : "This field is required.";
+      if ("endDate" in fieldValues)
+        temp.endDate = fieldValues.endDate ? "" : "This field is required.";
+      if ("startDate" in fieldValues)
+        temp.startDate = fieldValues.AssignmentName
+          ? ""
+          : "This field is required.";
+      if ("weightage" in fieldValues)
+        temp.weightage = fieldValues.weightage ? "" : "This field is required.";
+      setErrors({
+        ...temp
+      });
+  
+      if (fieldValues == values) return Object.values(temp).every((x) => x == "");
+    };
+  
     const {
-        TblContainer,
-        TblHead,
-        TblPagination
-    } = useTable(records, headCells, filterFn);
-
-
-    const addOrEdit = (assignment, resetForm) => {
-        if (assignment.id === 0)
-            assignmentService.insertAssignment(assignment)
-        else
-            assignmentService.updateAssignment(assignment)
-        resetForm()
-        setRecordForEdit(null)
-        setOpenPopup(false)
-        setRecords(assignmentService.getAllAssignments())
-    }
-
-    const handleCellClick = (item) => {
-        openInPopup(item);
-    }
-
+      values,
+      setValues,
+      errors,
+      setErrors,
+      handleInputChange,
+      resetForm
+    } = useForm(initialFValues, true, validate);
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (validate()) {
+        addOrEdit(values, resetForm);
+      }
+    };
+  
+    useEffect(() => {
+      if (recordForEdit != null)
+        setValues({
+          ...recordForEdit
+        });
+    }, [recordForEdit]);
+  
     return (
-        <>
-            <AssignmentForm
-                recordForEdit={recordForEdit}
-                addOrEdit={addOrEdit} />
-        </>
-        
-    )
+      <Form onSubmit={handleSubmit}>
+        <Grid container>
+          <Grid item xs={6}>
+            <Controls.Input
+              name="AssignmentName"
+              label="Assignment Name"
+              value={values.AssignmentName}
+              onChange={handleInputChange}
+              error={errors.AssignmentName}
+            />
+            <Controls.Input
+              label="Weightage"
+              name="weightage"
+              value={values.weightage}
+              onChange={handleInputChange}
+            />
+            <Controls.DatePicker
+              name="startDate"
+              label="Start Date"
+              value={values.startDate}
+              onChange={handleInputChange}
+            />
+            <Controls.DatePicker
+              name="endDate"
+              label="End Date"
+              value={values.endDate}
+              onChange={handleInputChange}
+            />
+  
+            <div>
+              <Controls.Button type="submit" text="Submit" />
+              <Controls.Button text="Reset" color="default" onClick={resetForm} />
+            </div>
+          </Grid>
+        </Grid>
+      </Form>
+    );
+  }
