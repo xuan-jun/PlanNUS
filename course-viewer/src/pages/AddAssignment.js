6a498ce0e76fb9dc40e5726d4982bbef44ceb259
kohlongyang <longyangk10@gmail.com>
2023-03-26 22:29:38 +0800

    align centre

--- course-viewer/src/pages/AddAssignment.js
+++ course-viewer/src/pages/AddAssignment.js
@@ -65,7 +65,7 @@ export default function AssignmentForm(props) {
   return (
     <Form onSubmit={handleSubmit}>
         
-      <Grid container spacing={2} margin={2}>
+      <Grid container spacing={20} margin={20}>
         <Grid item xs={6}>
           <Controls.Input
             name="AssignmentName"
