6c0efba8fcda01bddc37cb82291d3abbec2b7721
kohlongyang <longyangk10@gmail.com>
2023-03-26 23:50:18 +0800

    add logo

--- course-viewer/src/components/Navbar/Navbar.jsx
+++ course-viewer/src/components/Navbar/Navbar.jsx
@@ -15,8 +15,9 @@ const Navbar = ({theme, setTheme}) => {
   return (
     <nav className="navBar">
       <div className="site-logo">
-        <img src="assets/logo.png" width="6%" height="6%">
-        <Link to='/'>Logo</Link>
+        <Link to='/'>
+          <img src="../../assets/logo.png" width="100%" height="100%"/>
+        </Link>
       </div>
       <div onClick={switchTheme} className='light-mode-toggle'>
         <div className={`slider ${theme === 'light' ? '-left' : '-right'}`}>
