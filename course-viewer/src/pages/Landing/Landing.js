543e8975aea4a1d008b5cdf3bf93f9712706f45e
kohlongyang <longyangk10@gmail.com>
2023-04-09 17:08:05 +0800

    removed TextContainer and just input everything into Landing.js

--- course-viewer/src/pages/Landing/Landing.js
+++ course-viewer/src/pages/Landing/Landing.js
@@ -2,9 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './Landing.css'
 import logo from '../../assets/logo.png'
 import axios from 'axios';
-import TextContainer from "../../components/TextContainer.tsx";
 import { motion } from "framer-motion";
-import { plannus, plannusWrapper, fadeIn } from "../../variants.ts";
+import { plannus, plannusWrapper, fadeIn, staggerContainer } from "../../variants.ts";
 
 function Landing() {
   const [assignments, setAssignments] = useState([]);
@@ -81,19 +80,28 @@ function Landing() {
 
   return <div className="landing-page">
     <body>
-
-    <TextContainer/>
-
-    <br></br>
+    <motion.div variants={staggerContainer} initial="initial" animate="animate">
+      {/* upper */}
+      <div>
+        <motion.span variants={fadeIn()} className="bubble-left">
+        <p><strong>Instructors!</strong> <br /> Have you wondered whether your students are 
+          having <br /> assignments on days where you have provided assignments?</p> 
+        </motion.span>
+        <motion.span variants={fadeIn()} className="bubble-right">
+        <p><strong>Students!</strong>  <br /> Have you wondered about how the assignment <br /> workloads are like
+        for the previous semester?</p>
+        </motion.span>
+      </div>
+    </motion.div>
 
     <motion.div variants={plannusWrapper} initial="initial" animate="animate" className="plannusWrapper">
       <motion.img src={logo} variants={plannus} className="plannus-logo" />
     </motion.div>
-
     <motion.span variants={fadeIn()} initial="initial" animate="animate">
         <p>Whether you are an instructor or a student, there is <strong>always</strong> something for you!</p>
-        <br></br>
     </motion.span>
+
+    <br></br><br></br>
 
     <div className="landing-select-type">
       <button class="learn-more">
@@ -104,8 +112,6 @@ function Landing() {
       </button>
     
     </div>
-
-    
 
     </body>  
   </div>;
